616f2d10ab83dc4d747d4bd2dcc0f723c037bd25
Pavlo Myrotiuk <pmyrotiuk@smartling.com>
2016-03-27 13:13:13 +0300

    implemented function to generate a list of random integers

--- src/main/scala-2.11/com/myrotiuk/chapter6/RNG.scala
+++ src/main/scala-2.11/com/myrotiuk/chapter6/RNG.scala
@@ -2,28 +2,48 @@ package com.myrotiuk.chapter6
 
 trait RNG {
   def nextInt: (Int, RNG)
+}
 
-  def positiveInt: (Int, RNG) = {
-    nextInt match {
-      case (result, generator) if (result == Int.MinValue) => generator.positiveInt
+object RNG {
+
+  def positiveInt(rng: RNG): (Int, RNG) = {
+    rng.nextInt match {
+      case (result, generator) if (result == Int.MinValue) => positiveInt(generator)
       case (result, generator) if (result < 0) => (result.abs, generator)
       case (result, generator) => (result, generator)
     }
   }
-}
 
-object RNG {
-  def double(rng: RNG): (Double, RNG) = ((rng.positiveInt._1 - 1).abs / Int.MaxValue, rng.positiveInt._2)
+  def double(rng: RNG): (Double, RNG) = ((positiveInt(rng)._1 - 1).abs / Int.MaxValue, positiveInt(rng)._2)
 
   def intDouble(rng: RNG): ((Int, Double), RNG) = {
-    val int: (Int, RNG) = rng.nextInt
-    val double: (Double, RNG) = RNG.double(int._2)
-    ((int, double), double._2)
+    val (r1, rng1) = positiveInt(rng)
+    val (r2, rng2) = double(rng1)
+    r1 -> r2 -> rng2
   }
 
-  def doubleInt(rng: RNG): ((Double, Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (r1, rng1) = double(rng)
+    val (r2, rng2) = positiveInt(rng1)
+    r1 -> r2 -> rng2
+  }
 
-  def double3(rng: RNG): ((Double, Double, Double), RNG) = ???
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (r1, rng1) = double(rng)
+    val (r2, rng2) = double(rng1)
+    val (r3, rng3) = double(rng2)
+    (r1, r2, r3) -> rng3
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count == 0) {
+      Nil -> rng
+    } else {
+      val (next, rng1) = positiveInt(rng)
+      val (intss, finalRng) = ints(count - 1)(rng1)
+      next::intss -> finalRng
+    }
+  }
 }
 
 object IndexRNG extends App {
