5267b563c5dfcf2d4a0757c0e4fccaf2329a4fcc
Pavlo Myrotiuk <pmyrotiuk@smartling.com>
2016-04-03 20:16:59 +0300

    added method that search full file path

--- src/main/scala-2.11/photo/FileSizeCalculator.scala
+++ src/main/scala-2.11/photo/FileSizeCalculator.scala
@@ -2,8 +2,7 @@ package photo
 
 import java.io.File
 
-
-object FileSizeCalculator  extends App {
+object FileSizeCalculator extends App {
 
   val rootPath = "/home/pavlo/Pictures/Photo"
   private val MEGABYTE = 1024 * 1024
@@ -13,15 +12,36 @@ object FileSizeCalculator  extends App {
     dirFiles ++ dirFiles.filter(_.isDirectory).flatMap(fileList)
   }
 
-  def groupByFileType(files: Array[File]) = {
-    files.filter(!_.isDirectory).groupBy(_.getName.split("\\.").last.toUpperCase)
+  def analyzeSize(dirName: String) = {
+
+    def groupByFileType(files: Array[File]) = {
+      files.filter(!_.isDirectory).groupBy(_.getName.split("\\.").last.toUpperCase)
+    }
+
+    groupByFileType(fileList(new File(rootPath))).map(
+      ext => (ext._1, ext._2.foldLeft(0L)((sum, file) => sum + file.length()))
+    ).mapValues(_ / MEGABYTE)
   }
 
-  private val fileType: Map[String, Array[File]] = groupByFileType(fileList(new File(rootPath)))
-  private val stringToLong: Map[String, Long] = fileType.map(
-    ext => (ext._1, ext._2.foldLeft(0L)((sum, file) => sum + file.length()))
-  )
-  val extToMb = stringToLong.mapValues(_ / MEGABYTE)
-  println(extToMb)
+  def locateFiles(fileNames: List[String], parentDir: String): List[String] =
+    fileList(new File(parentDir)).toList.filter(file => fileNames.contains(file.getName)).map(_.getPath)
+
+  println(locateFiles(List("IMG_3929.JPG",
+    "IMG_2983.JPG",
+    "IMG_3008.JPG",
+    "IMG_9565.JPG",
+    "IMG_8954.JPG",
+    "IMG_9376.JPG",
+    "IMG_9901.JPG",
+    "IMG_4388.JPG",
+    "MVI_5334.MOV",
+    "IMG_7202.JPG",
+    "456.jpg",
+    "736.jpg",
+    "741.jpg",
+    "753.jpg",
+    "q53.jpg",
+    "020520111683.jpg"), rootPath).mkString("\n"))
+
 
 }
